a15674e2c909973a4a350a4c9f13037c30b20068
charlesgao <63110137@qq.com>
2026-07-02 00:51:31 +0800

    使用markdown文档格式

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
-1.This is a test repository
-2.Test again
-3.It's successful
-4.add a function
-5.add another class
-6.test again
-7.test1
-8.test2
-9.test3
-10.test4
-11.test5
+# Cursor
 
-12.test6
-13.test7
-14.test8
-15.test9
+## 二、进阶篇
+
+### 4.CursorTab
+
+### 5.Agent基础功能
+
+### 6.Agent进阶功能
+
+### 7.AI聊天功能与应用
+
+### 8.Barckground Agent
+
+
+
+## 三、上下文工程篇
+
+### 9.Inline Edit
+#### 9.1 Quick Edit(Edit and Generate): Ctrl + K
+选中代码，表示修改此处代码(Edit)。如：帮我提供这个函数的另外一种写法
+不选中代码，表示在该位置新增代码(Generate)。如：提示输入一个数字n，打印“您输入的数字为：{n}”；若输入数字0，则退出///若输入的不是数字，加上错误处理
+
+#### 9.2 Quick Question: Alt + Enter (在Ctrl + K之后的窗体显示之后才能操作)
+针对选中的代码快速询问它的功能、用法、可能的改进方向
+若同意，可以输入：do it
+
+#### 9.3 Ctrl + Shift + Enter???
+
+#### 9.4 Add to Chat: Ctrl + L
+显示最近5次的git提交记录
+显示最近5次的git提交记录，只要作者和说明
+
+
--- test1.py
+++ test1.py
@@ -40,33 +40,51 @@ user = get_user_info()
 # print(x)
 
 # for i in range(1,4):
-#     # print(i)
-#     for j in range(i):
+#     for j in range(0,i):
 #         print(i, end=" ")
+#         j += 1
+#     i += 1
+
+
+
+while True:
+    try:
+        n = int(input("请输入一个数字: "))
+    except ValueError:
+        print("输入无效，请输入一个数字。")
+        continue
+    if n == 0:
+        break
+    print(f"您输入的数字是: {n}")
+
+
+# i = 1
+# while i < 4:
+#     j = 0
+#     while j < i:
+#         print(i, end=" ")
+#         j += 1
+#     i += 1
 
 # for i in range(1,4):
 #     print(i, end=" ")
 
-i = 0
-for j in range(i):
-    print(i, end=" ")
+# i = 1
+# for j in range(i):
+#     print(i, end=" ")
 
-i = 1
-for j in range(i):
-    print(i, end=" ")
+# i = 2
+# for j in range(i):
+#     print(i, end=" ")
 
-i = 2
-for j in range(i):
-    print(i, end=" ")
+# i = 3
+# for j in range(i):
+#     print(i, end=" ")
 
-i = 3
-for j in range(i):
-    print(i, end=" ")
+# # a = range(1)
+# # print(a)
 
 # a = range(1)
 # print(a)
-
-a = range(1)
-print(a)
-print(type(a))
-print(list(a))
+# print(type(a))
+# print(list(a))
